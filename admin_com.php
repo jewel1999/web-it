452dc5451650dfcc08e33487084c5745a5dc4ffb
jewel1999 <kimnaomi98@gmail.com>
2022-06-02 14:24:13 +0700

    commit edit_com but not update success

--- admin_com.php
+++ admin_com.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+    require_once 'connect_db.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +20,9 @@
         <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Insert Computer</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">   <!-- form for insert data -->
+        </div>    
+
+        <div class="modal-body">   <!-- form for insert data -->        
             <form action="admin_com_insert.php" method="post" enctype="multiplepart/form-data">
             <div class="mb-3">
                 <label for="com_sn" class="col-form-label">Compuster serial-number :</label>
@@ -40,16 +43,24 @@
 
             <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="submit" name="submit" class="btn btn-success">submit</button>
+            <button type="submit" name="com_insert" class="btn btn-success">submit</button>
         </div>
             </form>
+           
         </div>
         
         </div>
     </div>
     </div>
 
+    <!-- -------------------------- table section-------------------------- -->
+
+    
     <div class="container mt-3">
+    <div class="md-4  d-flex ">
+                <button href="admin.php" type="button" class="btn btn-dark" > back</button>
+            </div>
+            <br>
         <div class="row">
             <div class="col-md-6">
                     <h1> Computer information </h1>
@@ -57,8 +68,72 @@
             <div class="col-md-6  d-flex justify-content-end">
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#UserModal">Insert computer </button>
             </div>
+
+            
+
         </div>
-    </div>
+        <hr>
+        <br>
+        <?php if(isset($_SESSION['success'])) { ?>   <?php ?>
+                <div class="alert alert-success" role="alert">  
+            <?php 
+                echo $_SESSION['success'];
+                unset($_SESSION['success']); ?>
+                </div>
+            <?php } ?>
+
+            <?php if(isset($_SESSION['error'])) { ?>
+                <div class="alert alert-danger" role="alert">  
+                <?php 
+                    echo $_SESSION['error'];
+                    unset($_SESSION['error']); ?>
+                    </div>
+                <?php } ?>
+
+
+    <!--  --------------User data---------------------- -->
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">computer serial-number</th>
+            <th scope="col">computer name</th>
+            <th scope="col">owner</th>
+            <th scope="col">status</th>
+            <th scope="col">created time</th>
+            <th scope="col">action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                $stmt =$conn->query("SELECT * FROM computers");
+                $stmt->execute(); 
+                $computer_table = $stmt->fetchALL();
+
+                if(!$computer_table){
+                    echo"<tr> <td colpan='6' class='text-center'> No data found </td> </tr>";
+                }else{
+                    foreach  ($computer_table as $computers) {   // foreach = loop data in table
+            ?>
+            <tr>
+                <th scope="row"><?php echo $computers['id']; ?> </th>
+                <td><?php echo $computers['com_sn']; ?>     </td>
+                <td><?php echo $computers['com_name']; ?>   </td>
+                <td><?php echo $computers['com_owner']; ?>  </td>
+                <td><?php echo $computers['com_status']; ?> </td>
+                <td><?php echo $computers['create_at']; ?>  </td>       
+                <td> 
+                      
+                     <a href="edit_com.php?id=<?= $computers['id']; ?>"  class="btn btn-warning" >Edit</a>
+                     <a href="delete.php?id=<?= $computers['id']; ?>"  class="btn btn-danger" onclick="return confirm('are you sure to delete ?')" >Delete</a>
+                </td>    
+            </tr>
+        <?php }} ?>   
+        </tbody>
+    </table>
+            
+</div>
+            
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    
--- admin_com_insert.php
+++ admin_com_insert.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
     require_once "connect_db.php";
-    if(isset($_POST['submit'])){
+    if(isset($_POST['com_insert'])){
         $com_sn = $_POST['com_sn'];
         $com_name = $_POST['com_name'];
         $com_owner = $_POST['com_owner'];
@@ -9,6 +9,11 @@
 
         if(!isset($_SESSION['error'])){
         try {
+            $check_com_sn = $conn->prepare("SELECT com_sn FROM computers WHERE com_sn = :com_sn");   // : replace values
+            $check_com_sn->bindParam(":com_sn",$com_sn);
+            $check_com_sn->execute();
+            $row = $check_com_sn->fetch(PDO::FETCH_ASSOC);
+
                 $stmt = $conn->prepare("INSERT INTO computers(com_sn,com_name,com_owner,com_status)
                                         VALUES (:com_sn,:com_name,:com_owner,:com_status)");
                 $stmt->bindParam(":com_sn",$com_sn);
@@ -16,12 +21,13 @@
                 $stmt->bindParam(":com_owner",$com_owner);
                 $stmt->bindParam(":com_status",$com_status);
                 $stmt->execute();
-                $_SESSION['success'] = " Successfully insert computer information ! <a href='admin_com_insert.php' class='alert alert-link' > Click here </a> " ;
+                $_SESSION['success'] = "data has been inserted sucessfully! " ;
                 header("location:admin_com.php");
+            
             }catch(PDOException $e){    
-                echo $e->getMessage();
-            }
+            echo $e->getMessage();
         }
-        
     }
+}
+
 ?>
